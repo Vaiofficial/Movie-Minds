d4cd99136932384bea1bd0e257f689abf024e2d0
P Vaibhav <pvaibhavofficial@gmail.com>
2023-11-12 00:54:40 +0530

    created header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from '@/Components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* header */}
+        <Header/>
+
+
+        {/* navbar */}
+
+
+
+        {/* searchBox */}
+        
+        
+        {children}</body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,6 @@ export default function Home() {
   return (
     <>
     <h1 className=' text-red-500'>Hello</h1>
-    </>
+    </> 
   )
 }
