4d649cfa001f2d00a58be4232dd92bde0e5c0305
P Vaibhav <pvaibhavofficial@gmail.com>
2023-11-16 17:31:28 +0530

    created about page

---
+++ src/app/about/page.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 ">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to Movie Dekho! I love movies, and I made this website to share
+        that love with you. Here, you can explore a huge collection of films, TV
+        shows, and learn about the talented people who make them. Whether you're
+        just a casual viewer or a big movie fan, Movie Dekho is made for you. We
+        want to make it easy for you to find what you're looking for by
+        providing details, reviews, and ratings. Join us in the world of Movie
+        Dekho, where the joy of movies is endless, and there's excitement in
+        every click.
+      </p>
+
+      <p>
+        But Movie Dekho is more than just a collection of films; it's a place to
+        share your thoughts and connect with fellow movie enthusiasts. We
+        encourage you to dive into the reviews and ratings, and maybe even
+        contribute your own. Let's build a community where everyone's voice is
+        heard, and where the joy of discussing movies is as fulfilling as
+        watching them.
+      </p>
+
+      <p>
+        Our commitment extends beyond the screen – we want to empower you to
+        make informed choices about what to watch next. The detailed
+        information, thoughtful reviews, and user ratings are here to assist you
+        on your cinematic journey. Consider Movie Dekho your co-pilot, guiding
+        you through the vast landscape of film with expertise and enthusiasm.
+      </p>
+
+      <p>
+        So, whether you're here to discover new films, engage in discussions, or
+        simply revel in the joy of movies, Movie Dekho welcomes you with open
+        arms. Join us as we celebrate the magic of cinema, one click at a time.
+      </p>
+    </div>
+  );
+}
