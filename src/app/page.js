bce806bce1a01c63ef7cab8f8447bb4f64913fb5
P Vaibhav <pvaibhavofficial@gmail.com>
2023-11-17 17:51:30 +0530

    fetch data from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,28 @@
-import Image from 'next/image'
+import Results from "@/Components/Results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("failed to fetch data");
+  }
+
+  const data = await res.json();
+  const results = data.results;
+
+  // console.log(results);
+
   return (
-    <>
-    </> 
-  )
+    <div>
+      <Results results={results}></Results>
+    </div>
+  );
 }
