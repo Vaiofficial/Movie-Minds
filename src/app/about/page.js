388440cc64973db6669f103349b49e2fa17be0c2
P Vaibhav <pvaibhavofficial@gmail.com>
2023-11-18 16:26:43 +0530

    fix some issue for deployment

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,21 +5,21 @@ export default function About() {
     <div className="max-w-6xl mx-auto space-y-4 p-4 ">
       <h1 className="text-2xl font-medium text-amber-600">About</h1>
       <p>
-        Welcome to Movie Dekho! I love movies, and I made this website to share
+      Welcome to Movie Dekho! I love movies, and I made this website to share
         that love with you. Here, you can explore a huge collection of films, TV
-        shows, and learn about the talented people who make them. Whether you're
+        shows, and learn about the talented people who make them. Whether you&apos;re
         just a casual viewer or a big movie fan, Movie Dekho is made for you. We
-        want to make it easy for you to find what you're looking for by
+        want to make it easy for you to find what you&apos;re looking for by
         providing details, reviews, and ratings. Join us in the world of Movie
-        Dekho, where the joy of movies is endless, and there's excitement in
+        Dekho, where the joy of movies is endless, and there&apos;s excitement in
         every click.
       </p>
 
       <p>
-        But Movie Dekho is more than just a collection of films; it's a place to
+        But Movie Dekho is more than just a collection of films; it&apos;s a place to
         share your thoughts and connect with fellow movie enthusiasts. We
         encourage you to dive into the reviews and ratings, and maybe even
-        contribute your own. Let's build a community where everyone's voice is
+        contribute your own. Let&apos;s build a community where everyone&apos;s voice is
         heard, and where the joy of discussing movies is as fulfilling as
         watching them.
       </p>
@@ -33,7 +33,7 @@ export default function About() {
       </p>
 
       <p>
-        So, whether you're here to discover new films, engage in discussions, or
+        So, whether you&apos;re here to discover new films, engage in discussions, or
         simply revel in the joy of movies, Movie Dekho welcomes you with open
         arms. Join us as we celebrate the magic of cinema, one click at a time.
       </p>
--- src/app/error.js
+++ src/app/error.js
@@ -1,9 +1,8 @@
 "use client";
 //as we are using useEffect and onclick also so we need to include "use client".
-
 import React, { useEffect } from "react";
 
-export default function error({ error, reset }) {
+export default function Error({ error, reset }) {
   useEffect(() => {
     console.log(error);
   }, [error]);
@@ -11,7 +10,8 @@ export default function error({ error, reset }) {
   return (
     <div className="text-center mt-10 ">
       <h1>Something went wrong!</h1>
-      <button className="hover:text-amber-600 bg-amber-200 rounded-md px-3 py-2 mt-3 text-gray-700"
+      <button
+        className="hover:text-amber-600 bg-amber-200 rounded-md px-3 py-2 mt-3 text-gray-700"
         onClick={() => {
           reset();
         }}
