4c3248d20b95c1c5a65fcbf2f3a60c267e9f7e17
P Vaibhav <pvaibhavofficial@gmail.com>
2023-11-18 11:02:35 +0530

    added card component

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Card from "./Card";
 
 export default function Results({ results }) {
-  // console.log(results.original_title)
+  console.log(results);
   return (
-    <div>
-      {results.map((result) => {
-        return <div key={result.id}>{result.original_title}</div>;
-      })}
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
+      {results.map((result) => (
+        <Card key={result.id} result={result}></Card>
+      ))}
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default async function Home({ searchParams }) {
   const data = await res.json();
   const results = data.results;
 
-  console.log(results);
+  // console.log(results);
 
   return (
     <div>
