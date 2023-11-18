f8401f8636937ad2e101d701027911c640d6808e
P Vaibhav <pvaibhavofficial@gmail.com>
2023-11-18 15:48:45 +0530

    Changed favicon & header logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -21,7 +21,7 @@ export default function Header() {
       <div className='flex items-center space-x-5'>
         <DarkModeSwitch/>
         <Link href="/">
-            <h2 className='text-2xl'><span className='font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1'>IMDb</span><span className='text-xl hidden sm:inline'>Clone</span>
+            <h2 className='text-2xl'><span className='font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1'>Movie</span><span className='text-xl hidden sm:inline'>Minds</span>
             </h2>
         </Link>
       </div>
